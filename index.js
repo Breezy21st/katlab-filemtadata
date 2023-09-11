0239c7c1e4963ea10a89639903aa156b93ad3046
Breezy21st <110764642+Breezy21st@users.noreply.github.com>
2023-09-11 14:31:29 +0200

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   uploads/upfile-1694435327920.sql 	new file:   uploads/upfile-1694435481401.sql 	new file:   uploads/upfile-1694435485992

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+var multer = require('multer');
+var path = require('path');
 
 var app = express();
 
@@ -11,10 +13,27 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Configure Multer to handle file uploads
+var storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
 
+var upload = multer({ storage });
 
+// Handle file upload and send JSON response
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  var { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size: size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
